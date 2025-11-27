6080ab820bdfad4de77848f7e0c7a9252917a6f7
Nasadude <mike@technating.io>
2025-11-26 23:31:56 -0600

    Added class for SolarSystemBodies and with planets

--- solar_system.py
+++ solar_system.py
@@ -8,6 +8,40 @@ from random import randint
 pg.init()
 
 BLACK = (0,0,0)
+YELLOW = (255, 255, 0)
+GRAY   = (128, 128, 128)
+YELLOWISH_WHITE = (255, 255, 246)
+BLUE = (0, 0, 255)
+RED = (188, 39, 50)
+
+
+# Class Solar System Bodies
+class SolarSystemBodies:
+
+    AU = 1.496e11
+    SCALE = 285 / AU
+
+    # Constructor
+    def __init__(self, name, color, x, y, mass, radius):
+        self.name = name
+        self.color = color
+        self.x = x
+        self.y = y
+        self.mass = mass
+        self.radius = radius
+
+    # Method 1 - Draw the bodies on the Simulator
+    def draw_body(self, WINDOW):
+        x = self.x * SolarSystemBodies.SCALE + WIDTH//2
+        y = self.y * SolarSystemBodies.SCALE + HEIGHT//2
+
+        pg.draw.circle(
+            surface=WINDOW,
+            color=self.color,
+            center=(x, y),
+            radius=self.radius
+        )
+
 
 # Create Window
 screen_info = pg.display.Info()
@@ -38,6 +72,21 @@ def draw_stars(stars_list):
             star['radius']
         )
 
+sun = SolarSystemBodies("Sun", YELLOW, 0, 0, 1.989e30, 30)
+
+mercury = SolarSystemBodies(
+    "Mercury",
+    GRAY,
+    0.39 * SolarSystemBodies.AU,
+    0,
+    0.33e24,
+    6
+)
+venus = SolarSystemBodies("Venus", YELLOWISH_WHITE, 0.72 * SolarSystemBodies.AU, 0, 4.87e24, 14)
+earth = SolarSystemBodies("Earth", BLUE, 1 * SolarSystemBodies.AU, 0, 5.97e24, 15)
+mars = SolarSystemBodies("Mars", RED, 1.52 * SolarSystemBodies.AU, 0, 0.642e24, 8)
+
+
 
 # Create Simulation
 run = True
@@ -50,6 +99,10 @@ while run:
         if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
             run = False
 
+    ss_bodies = [sun, mercury,venus,earth,mars]
+
+    for body in ss_bodies:
+        body.draw_body(WINDOW)
     pg.display.update()
 
 # Quit the Pygame
