4bc761455881a8291924fbd98d21841f3d02ce84
Nasadude <mike@technating.io>
2025-11-26 22:38:53 -0600

    Added pygame window for simulation

--- solar_system.py
+++ solar_system.py
@@ -2,3 +2,32 @@
 import pygame as pg
 import math
 from random import randint
+
+# Initialize pygame
+
+pg.init()
+
+BLACK = (0,0,0)
+
+# Create Window
+screen_info = pg.display.Info()
+WIDTH = screen_info.current_w
+HEIGHT = screen_info.current_h
+WINDOW = pg.display.set_mode((WIDTH-150, HEIGHT-150))
+pg.display.set_caption("Solar System Simulator")
+
+# Create Simulation
+run = True
+while run:
+
+    WINDOW.fill(BLACK)
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            run = False
+
+    pg.display.update()
+
+# Quit the Pygame
+pg.quit()
+
